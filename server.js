20265b78eb4fe12ffd06a1c7fa1369297bec5f36
Yahnych <41375402+Yahnych@users.noreply.github.com>
2020-03-01 13:59:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const WebSocket = require('ws'),
     logger = require("./core/logger.js"),
     config = require('./config.json');
 
-const facebookHandler = require("./fstyleModules/facebookHandler.js");
+//const facebookHandler = require("./fstyleModules/facebookHandler.js");
 const userBots = [];
 let userWS = null,
     stoppingBots = false,
